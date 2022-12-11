405678db9681c3cb539e25e9c46d9956065af8dd
Murat Can Özhan <84957193+muratcanzhan@users.noreply.github.com>
2022-12-11 17:38:48 +0300

    Fikstür Oluşturucu
    Fikstür Oluşturucu
    
    
    Java ile girilen takımlar için rastgele maç fikstürü oluşturan bir sınıf yazılmalı.
    
    
    
    Kurallar :
    
    
    
    Çift Devreli Lig usülü uygulanacaktır. Her takım diğer takımlarla kendi evinde ve deplasmanda olmak üzere iki maç yapacaktır.
    
    
    Listenin sol tarafı ev sahibini sağ tarafı deplasman takımını gösterir.
    
    
    Eğer tek sayıda takım listesi girilirse, çift sayıya tamamlanacak şekilde "Bay" adında bir takım daha eklenmeli. Bay ile eşleşen takımlar o hafta maç yapmayacağı anlamına gelir.

---
+++ Maç Fixtürü Oluşturucu/TeamsController.java
@@ -0,0 +1,105 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+public class TeamsController {
+    List<String> list = new LinkedList<>();
+
+
+    public void ligFixture() {
+
+
+        if (list.size() % 2 != 0) {
+            list.add("Bay");
+        }
+
+        if (list.size() != 0) {
+            System.out.println("The Teams");
+            for (String a : list) {
+                System.out.println("-" + a);
+            }
+
+
+        } else {
+            System.out.println("No teams");
+        }
+        System.out.println();
+        round();
+
+    }
+
+
+    private void round() {
+
+        List<String> listWeek = new LinkedList<>();
+
+
+        int round = 2 * (list.size() - 1);
+
+
+        List<String> backUpList = new LinkedList<>(list);
+
+
+        int value = (list.size() / 2);
+
+
+        for (int i = 1; i < round / 2 + 1; i++) {
+
+            for (int k = 0; k <= value + 2; k += 2) {
+                listWeek.add(backUpList.get(k) + " " + backUpList.get(k + 1));
+            }
+
+
+            backUpList.add(1, backUpList.get(value));
+            backUpList.add(value, backUpList.get(backUpList.size() - 1));
+            backUpList.remove(backUpList.size() - 1);
+            backUpList.remove(backUpList.size() - 2);
+
+        }
+
+
+        for (int i = round / 2 + 2; i <= round + 1; i++) {
+            for (int k = 0; k <= value + 2; k += 2) {
+                listWeek.add(backUpList.get(k + 1) + " " + backUpList.get(k));
+            }
+
+
+            backUpList.add(1, backUpList.get(value));
+            backUpList.add(value, backUpList.get(backUpList.size() - 1));
+            backUpList.remove(backUpList.size() - 1);
+            backUpList.remove(backUpList.size() - 2);
+        }
+
+        int count = 0;
+        int num;
+
+
+        for (int i = 1; i <= round; i++) {
+
+            System.out.println("***** " + i + " *****");
+
+            List<String> backup = new LinkedList<>();
+
+            for (int n = count; n < count + value; n++) {
+
+                backup.add(listWeek.get(n));
+
+            }
+
+            for (int m = 0; m < value; m++) {
+
+                num = new Random().nextInt(backup.size());
+                System.out.println(backup.get(num));
+                backup.remove(num);
+
+
+            }
+
+            count += value;
+
+        }
+
+
+    }
+
+}
